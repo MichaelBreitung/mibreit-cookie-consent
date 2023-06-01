3985b23e5007abf3c0ba3012d061d83faf1ea474
MichaelBreitung <mibreit@gmx.de>
2023-06-01 15:34:37 +0200

    medium: fixed tree-shaking - built lib now smaller

---
+++ lib-es6/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @author Michael Breitung
+ * @copyright Michael Breitung Photography (www.mibreit-photo.com)
+ */
+export { default as createCookieSelector } from './factories/createCookieSelector';
