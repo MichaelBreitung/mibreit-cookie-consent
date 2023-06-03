47d0cc8608f621aa8882dd962a3f95dbd20e9a98
MichaelBreitung <mibreit@gmx.de>
2023-06-03 12:42:23 +0200

    minor: cleanup

--- lib-es6/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @author Michael Breitung
- * @copyright Michael Breitung Photography (www.mibreit-photo.com)
- */
-export { default as createCookieConsent } from './factories/createCookieConsent';
-export { getConsentCookie } from './containers/CookieConsent';
